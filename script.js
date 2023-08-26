d50f9b9322c9e903ae2ab792207d3eab8093ccd0
skka3134 <132555831+skka3134@users.noreply.github.com>
2023-08-27 02:25:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const searchButton = document.getElementById("searchButton");
+    const searchInput = document.getElementById("searchInput");
+    const searchEngine = document.getElementById("searchEngine");
+    const resultsDiv = document.getElementById("results");
+  
+    searchButton.addEventListener("click", function () {
+      const query = searchInput.value;
+      const selectedEngine = searchEngine.value;
+      let searchUrl = "";
+  
+      if (selectedEngine === "google") {
+        searchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+      } else if (selectedEngine === "bilibili") {
+        searchUrl = `https://search.bilibili.com/all?keyword=${encodeURIComponent(query)}`;
+      }else if (selectedEngine === "zhihu") {
+        searchUrl = `https://www.zhihu.com/search?q=${encodeURIComponent(query)}`;
+      }else if (selectedEngine === "youtube") {
+        searchUrl = `https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`;
+      } else if (selectedEngine === "douyin") {
+        searchUrl = ` https://www.douyin.com/search/${encodeURIComponent(query)}`;
+      } else if (selectedEngine === "tiktok") {
+        searchUrl =  `https://www.tiktok.com/search/user?q=${encodeURIComponent(query)}`;
+      } else if (selectedEngine === "weibo") {
+        searchUrl = `https://s.weibo.com/weibo/${encodeURIComponent(query)}`;
+      } else if (selectedEngine === "twitter") {
+        searchUrl =`https://twitter.com/search?q=${encodeURIComponent(query)}`;
+      } else if (selectedEngine === "jingdong") {
+        searchUrl = `https://search.jd.com/Search?keyword=${encodeURIComponent(query)}`;
+      } else if (selectedEngine === "taobao") {
+        searchUrl = `https://s.taobao.com/search?q=${encodeURIComponent(query)}`;
+      }
+  
+      if (searchUrl !== "") {
+        window.open(searchUrl, "_blank");
+      }
+    });
+  });
+  
